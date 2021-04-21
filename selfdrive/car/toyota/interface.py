93a9f64e22400ff483428abd260fd9ddb3f7bc23
Shane Smiskol <shane@smiskol.com>
2021-04-21 12:30:06 -0500

    allow engaging under 20 mph without pedal

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -413,10 +413,10 @@ class CarInterface(CarInterfaceBase):
     if self.CS.low_speed_lockout and self.CP.openpilotLongitudinalControl:
       events.add(EventName.lowSpeedLockout)
     if ret.vEgo < self.CP.minEnableSpeed and self.CP.openpilotLongitudinalControl:
-      events.add(EventName.belowEngageSpeed)
-      if c.actuators.gas > 0.1:
-        # some margin on the actuator to not false trigger cancellation while stopping
-        events.add(EventName.speedTooLow)
+      # events.add(EventName.belowEngageSpeed)
+      # if c.actuators.gas > 0.1:
+      #   # some margin on the actuator to not false trigger cancellation while stopping
+      #   events.add(EventName.speedTooLow)
       if ret.vEgo < 0.001:
         # while in standstill, send a user alert
         events.add(EventName.manualRestart)
