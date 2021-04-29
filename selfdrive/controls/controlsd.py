4dc16937cde5022793085e1bc970cc034d2edf5c
Shane Smiskol <shane@smiskol.com>
2021-04-28 20:55:35 -0500

    Don't show noGps alert for now

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -258,11 +258,11 @@ class Controls:
       self.events.add(EventName.fcw)
 
     # TODO: fix simulator
-    if not SIMULATION:
-      if not NOSENSOR and not self.support_white_panda:
-        if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and not TICI:
-          # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
-          self.events.add(EventName.noGps)
+    # if not SIMULATION:
+    #   if not NOSENSOR and not self.support_white_panda:
+    #     if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000) and not TICI:
+    #       # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
+    #       self.events.add(EventName.noGps)
       if not self.sm.all_alive(['roadCameraState', 'driverCameraState']) and (self.sm.frame > 5 / DT_CTRL):
         self.events.add(EventName.cameraMalfunction)
       if self.sm['modelV2'].frameDropPerc > 20:
