a8b4db86590a7356556ec513d813a8b7959fe733
killinen <37126045+killinen@users.noreply.github.com>
2021-09-15 15:30:51 +0300

    Update stopping_condition

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -9,7 +9,7 @@ LongCtrlState = log.ControlsState.LongControlState
 #STOPPING_EGO_SPEED = 0.5    # Original value
 STOPPING_EGO_SPEED = 5.
 #STOPPING_TARGET_SPEED_OFFSET = 0.01
-STOPPING_TARGET_SPEED_OFFSET = 2.01
+STOPPING_TARGET_SPEED_OFFSET = 3.01
 STARTING_TARGET_SPEED = 0.5
 BRAKE_THRESHOLD_TO_PID = 0.2
 
@@ -23,7 +23,7 @@ def long_control_state_trans(active, long_control_state, v_ego, v_target, v_pid,
   """Update longitudinal control state machine"""
   stopping_target_speed = min_speed_can + STOPPING_TARGET_SPEED_OFFSET
 #  stopping_condition = (v_ego < 2.0 and cruise_standstill) or \
-  stopping_condition = (v_ego < 2.0) or \
+  stopping_condition = (v_ego < 3.0) or \
                        (v_ego < STOPPING_EGO_SPEED and
                         ((v_pid < stopping_target_speed and v_target < stopping_target_speed) or
                          brake_pressed))
