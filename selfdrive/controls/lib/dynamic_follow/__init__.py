e576ec88c2c1fe9056e786337ba91f215617349b
killinen <37126045+killinen@users.noreply.github.com>
2021-09-18 15:44:41 +0300

    Modify sng.stuff

--- selfdrive/controls/lib/dynamic_follow/__init__.py
+++ selfdrive/controls/lib/dynamic_follow/__init__.py
@@ -305,7 +305,8 @@ class DynamicFollow:
       TR = interp(self.car_data.v_ego, x_vel, y_dist)
     else:  # this allows us to get closer to the lead car when stopping, while being able to have smooth stop and go when reaccelerating
       self.sng = True
-      x = [self.sng_speed * 0.7, self.sng_speed]  # decrease TR between 12.6 and 18 mph from 1.8s to defined TR above at 18mph while accelerating
+      #x = [self.sng_speed * 0.7, self.sng_speed]  # decrease TR between 12.6 and 18 mph from 1.8s to defined TR above at 18mph while accelerating
+      x = [self.sng_speed * 1., self.sng_speed]
       y = [self.sng_TR, interp(self.sng_speed, x_vel, y_dist)]
       TR = interp(self.car_data.v_ego, x, y)
 
