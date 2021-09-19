fcdf6b9904aebb04d28c8d8c9975a8f77fe88b10
killinen <37126045+killinen@users.noreply.github.com>
2021-09-19 19:17:57 +0300

    Make car accerate more on high speed

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -27,8 +27,10 @@ _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
-_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
+_A_CRUISE_MAX_V = [1.2, 1.2, 0.8, .8]
+#_A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
+_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.8, .8]
+#_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
 # _A_CRUISE_MAX_V = [1.6, 1.4, 0.7, .4]
 # _A_CRUISE_MAX_V_FOLLOWING = [1.7, 1.65, 0.7, .5]
 _A_CRUISE_MAX_BP = [0.,  6.4, 22.5, 40.]
