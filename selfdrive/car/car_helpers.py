01b502974466d721667bb114ab7772e0399a6f1b
killinen <37126045+killinen@users.noreply.github.com>
2021-11-04 10:35:16 +0200

    Add more info in  car_helpers

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -177,6 +177,7 @@ def get_car(logcan, sendcan, has_relay=False):
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
     candidate = "CAR.OLD_CAR"
+    print("canditate is not None")
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
