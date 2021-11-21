b64fd19ec7cad2a9faae72837461b1a3036df33a
killinen <37126045+killinen@users.noreply.github.com>
2021-11-21 14:55:10 +0200

    Modify brake_max stuff

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -85,10 +85,7 @@ class LongControl():
     brake_max = interp(CS.vEgo, CP.brakeMaxBP, CP.brakeMaxV)
 
     if self.op_params.get('dynamic_gas'):
-      gas_max, lead = self.dynamic_gas.update(CS, extras)
-      
-    if lead:
-      brake_max = 0.1
+      gas_max, brake_max = self.dynamic_gas.update(CS, extras)
 
     # Update state machine
     output_gb = self.last_output_gb
