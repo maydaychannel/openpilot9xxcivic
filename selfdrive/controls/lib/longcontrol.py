0265e282bf36d9d7c34659ec91d54a85dc651b26
killinen <37126045+killinen@users.noreply.github.com>
2022-05-15 16:38:07 +0300

    Add v_ego < 10 to stopping_condition

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -29,7 +29,7 @@ def long_control_state_trans(active, long_control_state, v_ego, v_target, v_pid,
   stopping_target_speed = min_speed_can + STOPPING_TARGET_SPEED_OFFSET
 #  stopping_condition = (v_ego < 3.0) or \    #This doesn't work, itll stop but starting is not so good
 #  stopping_condition = (v_ego < 2.0 and cruise_standstill) or \
-  stopping_condition = (dRel < 2.0 and lead_car) or \
+  stopping_condition = (dRel < 2.0 and lead_car and v_ego < 10) or \
                        (v_ego < STOPPING_EGO_SPEED and
                         ((v_pid < stopping_target_speed and v_target < stopping_target_speed) or
                          brake_pressed))
