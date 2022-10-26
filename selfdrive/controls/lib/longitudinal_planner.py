7da3974335c8a9a045f5ebf0ac1078f814955a40
killinen <37126045+killinen@users.noreply.github.com>
2022-10-26 16:33:02 +0300

    Make accelaration arrays same
    Prolly gives jerky SnG bc max a_ego fluctuates between cruise_max and following

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -27,7 +27,8 @@ _A_CRUISE_MIN_BP = [  0.,  5.,  10., 20.,  40.]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [1.2, 1.2, 0.8, .8]
+#_A_CRUISE_MAX_V = [1.2, 1.2, 0.8, .8]    # Last values
+_A_CRUISE_MAX_V = [1.6, 1.6, 0.8, .8]
 #_A_CRUISE_MAX_V = [1.2, 1.2, 0.65, .4]
 _A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.8, .8]
 #_A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.6, 0.65, .4]
