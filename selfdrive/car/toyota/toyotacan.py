be58880d42c8f193bfefa8854d5b533b8bd12e69
killinen <37126045+killinen@users.noreply.github.com>
2023-05-02 21:51:50 +0300

    Find bug by reverting the steer_command to old

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -1,30 +1,30 @@
-# def create_steer_command(packer, steer, steer_req, raw_cnt):
-#   """Creates a CAN message for the Toyota Steer Command."""
-#
-#   values = {
-#     "STEER_REQUEST": steer_req,
-#     "STEER_TORQUE_CMD": steer,
-#     "COUNTER": raw_cnt,
-#     "SET_ME_1": 1,
-#   }
-#   return packer.make_can_msg("STEERING_LKA", 0, values)
+def create_steer_command(packer, steer, steer_req, raw_cnt):
+  """Creates a CAN message for the Toyota Steer Command."""
 
-def create_steer_command(packer, mode, steer_delta, steer_tq, frame):
-  """Creates a CAN message for the actuator STEERING_COMMAND"""
-  packer = CANPacker('ocelot_controls')
   values = {
-    "SERVO_COUNTER": frame % 0xF,
-    "STEER_MODE": mode,
-    "STEER_ANGLE": steer_delta,
-    "STEER_TORQUE": steer_tq,
+    "STEER_REQUEST": steer_req,
+    "STEER_TORQUE_CMD": steer,
+    "COUNTER": raw_cnt,
+    "SET_ME_1": 1,
   }
-  msg = packer.make_can_msg("STEERING_COMMAND", 0, values)
-  addr = msg[0]
-  dat  = msg[2]
+  return packer.make_can_msg("STEERING_LKA", 0, values)
 
-  values["SERVO_CHECKSUM"] = calc_checksum_8bit(dat, addr)
-
-  return packer.make_can_msg("STEERING_COMMAND", 0, values) #bus 2 is the actuator CAN bus
+#def create_steer_command(packer, mode, steer_delta, steer_tq, frame):
+#  """Creates a CAN message for the actuator STEERING_COMMAND"""
+#  packer = CANPacker('ocelot_controls')
+#  values = {
+#    "SERVO_COUNTER": frame % 0xF,
+#    "STEER_MODE": mode,
+#    "STEER_ANGLE": steer_delta,
+#    "STEER_TORQUE": steer_tq,
+#  }
+#  msg = packer.make_can_msg("STEERING_COMMAND", 0, values)
+#  addr = msg[0]
+#  dat  = msg[2]
+#
+#  values["SERVO_CHECKSUM"] = calc_checksum_8bit(dat, addr)
+#
+#  return packer.make_can_msg("STEERING_COMMAND", 0, values) #bus 2 is the actuator CAN bus
 
 def calc_checksum_8bit(work_data, msg_id): # 0xb8 0x1a0 0x19e 0xaa 0xbf
   checksum = msg_id
