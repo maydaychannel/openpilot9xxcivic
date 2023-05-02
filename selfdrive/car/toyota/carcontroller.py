eb1e0cc2aeecf4d2f3b6ca74fd6e3f8908dc03ef
killinen <37126045+killinen@users.noreply.github.com>
2023-05-02 22:06:25 +0300

    Try having the new and old steer_command

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -3,7 +3,7 @@ from common.numpy_fast import clip, interp
 from selfdrive.car import apply_toyota_steer_torque_limits, create_gas_command, make_can_msg
 from selfdrive.car.toyota.toyotacan import create_steer_command, create_ui_command, \
                                            create_accel_command, create_acc_cancel_command, \
-                                           create_fcw_command, create_lta_steer_command, create_lead_command
+                                           create_fcw_command, create_lta_steer_command, create_lead_command, create_new_steer_command
 from selfdrive.car.toyota.values import Ecu, CAR, STATIC_MSGS, NO_STOP_TIMER_CAR, TSS2_CAR, CarControllerParams, MIN_ACC_SPEED
 from opendbc.can.packer import CANPacker
 from common.op_params import opParams
@@ -158,7 +158,7 @@ class CarController():
       # Original steer_command
       can_sends.append(create_steer_command(self.packer, apply_steer, apply_steer_req, frame))
       # StepperServoCan steer_command
-      #can_sends.append(create_steer_command(self.packer, apply_steer_req, 0, steer_tq, frame)) 
+      can_sends.append(create_new_steer_command(self.packer, apply_steer_req, 0, 0, frame)) 
       if frame % 2 == 0 and CS.CP.carFingerprint in TSS2_CAR:
         can_sends.append(create_lta_steer_command(self.packer, 0, 0, frame // 2))
 
