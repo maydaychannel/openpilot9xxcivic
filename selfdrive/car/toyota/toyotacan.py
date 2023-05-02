988b21739a6ea4a2e9411351def950d1888ff701
killinen <37126045+killinen@users.noreply.github.com>
2023-05-02 21:02:27 +0300

    Change create_steer_command() for StepperServoCan

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -1,15 +1,40 @@
-def create_steer_command(packer, steer, steer_req, raw_cnt):
-  """Creates a CAN message for the Toyota Steer Command."""
+# def create_steer_command(packer, steer, steer_req, raw_cnt):
+#   """Creates a CAN message for the Toyota Steer Command."""
+#
+#   values = {
+#     "STEER_REQUEST": steer_req,
+#     "STEER_TORQUE_CMD": steer,
+#     "COUNTER": raw_cnt,
+#     "SET_ME_1": 1,
+#   }
+#   return packer.make_can_msg("STEERING_LKA", 0, values)
 
+def create_steer_command(mode, steer_delta, steer_tq, frame):
+  """Creates a CAN message for the actuator STEERING_COMMAND"""
+  packer = CANPacker('ocelot_controls')
   values = {
-    "STEER_REQUEST": steer_req,
-    "STEER_TORQUE_CMD": steer,
-    "COUNTER": raw_cnt,
-    "SET_ME_1": 1,
+    "SERVO_COUNTER": frame % 0xF,
+    "STEER_MODE": mode,
+    "STEER_ANGLE": steer_delta,
+    "STEER_TORQUE": steer_tq,
   }
-  return packer.make_can_msg("STEERING_LKA", 0, values)
+  msg = packer.make_can_msg("STEERING_COMMAND", 0, values)
+  addr = msg[0]
+  dat  = msg[2]
 
+  values["SERVO_CHECKSUM"] = calc_checksum_8bit(dat, addr)
 
+  return packer.make_can_msg("STEERING_COMMAND", 2, values) #bus 2 is the actuator CAN bus
+
+def calc_checksum_8bit(work_data, msg_id): # 0xb8 0x1a0 0x19e 0xaa 0xbf
+  checksum = msg_id
+  for byte in work_data: #checksum is stripped from the data
+    checksum += byte     #add up all the bytes
+
+  checksum = (checksum & 0xFF) + (checksum >> 8); #add upper and lower Bytes
+  checksum &= 0xFF #throw away anything in upper Byte
+  return checksum
+  
 def create_lta_steer_command(packer, steer, steer_req, raw_cnt):
   """Creates a CAN message for the Toyota LTA Steer Command."""
 
