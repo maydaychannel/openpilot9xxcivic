b7283e4787abe34a21d5568b28d16c7bc1b734af
maydaychannel <55064598+maydaychannel@users.noreply.github.com>
2024-07-28 17:52:51 -0600

    add cruise speed conversion

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -1,3 +1,4 @@
+from openpilot.comma.conversions import Conversions as CV
 def create_new_steer_command(packer, mode, steer_delta, steer_tq, frame):
   """Creates a CAN message for the actuator STEERING_COMMAND"""
 #  packer = CANPacker('ocelot_controls')
@@ -12,5 +13,7 @@ def create_new_steer_command(packer, mode, steer_delta, steer_tq, frame):
   dat  = msg[2]
 
   values["SERVO_CHECKSUM"] = calc_checksum_8bit(dat, addr)
-
   return packer.make_can_msg("STEERING_COMMAND", 0, values) #bus 2 is the actuator CAN bus
+
+def get_cruise_speed_conversion(car_fingerprint: str, is_metric: bool) -> float:
+  return CV.KPH_TO_MS
