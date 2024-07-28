4fe5416ea0ea2bdd09c582dbcda0a9c502529c5e
maydaychannel <55064598+maydaychannel@users.noreply.github.com>
2024-07-28 12:23:02 -0600

    add non-tested-cars

--- selfdrive/car/tests/routes.py
+++ selfdrive/car/tests/routes.py
@@ -2,12 +2,15 @@
 from typing import NamedTuple
 from openpilot.selfdrive.car.honda.values import CAR as HONDA
 from openpilot.selfdrive.car.values import Platform
-
+from openpilot.selfdrive.car.ford.values import CAR as FORD
 class CarTestRoute(NamedTuple):
   route: str
   car_model: Platform | None
   segment: int | None = None
 
+non_tested_cars = [
+  FORD.FORD_F_150_MK14,
+]
 
 routes = [
   CarTestRoute("a74b011b32b51b56|2020-07-26--17-09-36", HONDA.CIVIC_07),
