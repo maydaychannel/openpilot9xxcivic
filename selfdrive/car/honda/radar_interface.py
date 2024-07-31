0175091486d80690b80323b031dd24ffee1d471d
maydaychannel <55064598+maydaychannel@users.noreply.github.com>
2024-07-31 14:15:46 -0600

    Update radar_interface.py

--- selfdrive/car/honda/radar_interface.py
+++ selfdrive/car/honda/radar_interface.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-from cereal import car
-from opendbc.can.parser import CANParser
 from openpilot.selfdrive.car.interfaces import RadarInterfaceBase
-from openpilot.selfdrive.car.honda.values import DBC
 
 class RadarInterface(RadarInterfaceBase):
   pass
